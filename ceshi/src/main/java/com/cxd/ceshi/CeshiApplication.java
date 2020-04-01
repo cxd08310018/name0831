6bb3a04073a2d958ff6af86e18b1a8cb257cec83
15633104375 <cxd0831>
2020-04-01 09:42:18 +0800

    测试

---
+++ ceshi/src/main/java/com/cxd/ceshi/CeshiApplication.java
@@ -0,0 +1,13 @@
+package com.cxd.ceshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeshiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CeshiApplication.class, args);
+    }
+
+}
---
+++ ceshi/src/test/java/com/cxd/ceshi/CeshiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cxd.ceshi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeshiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
